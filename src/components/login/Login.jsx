bb9a403b34804b63fdf4b195d9bccdd292e33bdc
yjyvsdream <jingyuan.yu@myxxjs.com>
2021-06-22 10:20:03 +0800

    修改：去掉redux-form, 并将类组件改为 函数组件

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,109 +1,92 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
-import {reduxForm} from 'redux-form';
-import { Link as RouterLink} from "react-router-dom";
-import {Box,Button,Container,Grid,Link,TextField,Typography} from '@material-ui/core';
-import { LoginActionType } from '../../types';
+import {Link as RouterLink} from "react-router-dom";
+import {Box, Button, Container, Grid, Link, TextField, Typography} from '@material-ui/core';
 import {webName} from "../../config";
+
 const loginAction = require('../../actions/login/LoginAction');
 
+const Login = (props) => {
+    const {login} = props;
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+    const [submitFlag, setSubmitFlag] = useState(false);
 
-
-class Login extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    componentDidMount() {
-    }
-    login =()=>{
-        this.props.login(this.props.loginReducer)
-    }
-    render() {
-        const {login,loginReducer,changeUsername,changePassword} = this.props;
-        return (
-        <>
-            <Box sx={{
-                    backgroundColor: 'background.default',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: '100%',
-                    justifyContent: 'center',
-                    alignItems:"center"
-            }}>
-            <Container maxWidth="sm" style={{paddingTop:80}}>
-                <Typography color="textPrimary" variant="h2" align="center" ><img   style={{paddingTop:6}} src="/logo120.png" alt=""/></Typography>
+    return (
+        <Box sx={{
+            backgroundColor: 'background.default',
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: "center"
+        }}>
+            <Container maxWidth="sm" style={{paddingTop: 80}}>
+                <Typography color="textPrimary" variant="h2" align="center"><img style={{paddingTop: 6}} src="/logo120.png" alt=""/></Typography>
                 <Typography color="textPrimary" variant="h2" align="center">{webName}</Typography>
-                <form>
-                    <TextField fullWidth
-                        label="用户名"
-                        margin="normal"
-                        name="userName"
-                        variant="outlined"
-                        onChange={changeUsername}
-                        value={loginReducer.userName}
-                    />
-                    <TextField fullWidth
-                        label="密码"
-                        margin="normal"
-                        name="password"
-                        type="password"
-                        variant="outlined"
-                        onChange={changePassword}
-                        value={loginReducer.password}
-                    />
-                    <Box sx={{ py: 2 }}>
-                        <Button color="primary" fullWidth size="large" type="button" onClick={this.login} variant="contained"
-                        endIcon={<i className="mdi mdi-login" />}>
-                            登陆
-                        </Button>
-                    </Box>
-                    
-                    <Grid container spacing={3} >
-                        <Grid item xs={6}  >
-                            <Typography color="textSecondary" variant="body1">
-                                <Link component={RouterLink} to="/register" variant="h6">
-                                    注册
-                                </Link>
-                            </Typography>
-                        </Grid>
-                        <Grid container item xs={6}  direction="row" justify="flex-end" >
-                            <Typography color="textSecondary" variant="body1">
-                                <Link component={RouterLink} to="/reset" variant="h6">
-                                    忘记密码
-                                </Link>
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                </form>                        
-            </Container>
-            </Box>
-         </>   
-        )
-    }
-}
+                <TextField fullWidth
+                           label="用户名"
+                           margin="normal"
+                           name="userName"
+                           variant="outlined"
+                           onChange={(e) => {
+                               setSubmitFlag(false);
+                               setUserName(e.target.value)
+                           }}
+                           error={userName == "" && submitFlag}
+                           helperText={userName == "" && submitFlag  ? "用户名不能为空" : ""}
+                           value={userName}
+                />
+                <TextField fullWidth
+                           label="密码"
+                           margin="normal"
+                           name="password"
+                           type="password"
+                           variant="outlined"
+                           onChange={(e) => {
+                               setSubmitFlag(false);
+                               setPassword(e.target.value)
+                           }}
+                           error={password == "" && submitFlag}
+                           helperText={password == "" && submitFlag  ? "密码不能为空" : ""}
+                           value={password}
+                />
+                <Box sx={{py: 2}}>
+                    <Button color="primary" fullWidth size="large" type="button" onClick={() => {
+                        setSubmitFlag(true);
+                        login(userName, password)
+                    }} variant="contained" endIcon={<i className="mdi mdi-login"/>}>
+                        登陆
+                    </Button>
+                </Box>
 
-const mapStateToProps = (state) => {
-    return {
-        loginReducer: state.LoginReducer
-    }
+                <Grid container spacing={3}>
+                    <Grid item xs={6}>
+                        <Typography color="textSecondary" variant="body1">
+                            <Link component={RouterLink} to="/register" variant="h6">注册</Link>
+                        </Typography>
+                    </Grid>
+                    <Grid container item xs={6} direction="row" justify="flex-end">
+                        <Typography color="textSecondary" variant="body1">
+                            <Link component={RouterLink} to="/reset" variant="h6">忘记密码</Link>
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
+    )
+};
+
+const mapStateToProps = () => {
+    return {}
 };
 
 const mapDispatchToProps = (dispatch) => ({
-    login: (values) => {
-        dispatch(loginAction.login(values));
-    },
-    changeUsername:(e)=>{
-        dispatch(LoginActionType.setLoginUsername(e.target.value))
-    },
-    changePassword:(e)=>{
-        dispatch(LoginActionType.setLoginPassword(e.target.value))
+    login: (userName, password) => {
+        if (userName.length > 0 && password.length > 0) {
+            dispatch(loginAction.login({userName, password}));
+        }
     }
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(
-    reduxForm({
-            form: 'loginForm'
-        }
-    )(Login));
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
