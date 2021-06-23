8eba7e9939656f5cdc66536cfb44a1403e095e3c
yjyvsdream <jingyuan.yu@myxxjs.com>
2021-06-23 10:38:55 +0800

    修改：添加宽度属性，可以外层 设定 dialog 宽度

--- src/components/layout/SimpleModal.jsx
+++ src/components/layout/SimpleModal.jsx
@@ -1,19 +1,17 @@
-import React ,{Fragment,useEffect,useState}from 'react';
-import {AppBar,Dialog,Badge,Button,Fade,IconButton,Modal,Toolbar,Typography} from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
+import React, {Fragment, useEffect, useState} from 'react';
+import {Dialog, Typography} from '@material-ui/core';
+import {withStyles} from '@material-ui/core/styles';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import MuiDialogContent from '@material-ui/core/DialogContent';
 import MuiDialogActions from '@material-ui/core/DialogActions';
-
-import { makeStyles } from '@material-ui/core/styles';
 
 const styles = (theme) => ({
     root: {
         margin: 0,
         padding: theme.spacing(2),
-        backgroundColor:"#3f51b5",
-        color:"#FFF",
-        minWidth:400
+        backgroundColor: "#3f51b5",
+        color: "#FFF",
+        minWidth: 400
     },
     closeButton: {
         position: 'absolute',
@@ -24,19 +22,21 @@ const styles = (theme) => ({
 });
 
 const DialogTitle = withStyles(styles)((props) => {
-    const { children, classes,  ...other } = props;
+    const {children, classes, width, ...other} = props;
     return (
-      <MuiDialogTitle disableTypography className={classes.root} {...other}>
-        <Typography variant="h6">{children}</Typography>
-      </MuiDialogTitle>
+        <MuiDialogTitle disableTypography className={classes.root} style={{width: width}} {...other}>
+            <Typography variant="h6">{children}</Typography>
+        </MuiDialogTitle>
     );
 });
 
-const DialogContent = withStyles((theme) => {console.log(theme);return{
-    root: {
-        padding: theme.spacing(2),
+const DialogContent = withStyles((theme) => {
+    return {
+        root: {
+            padding: theme.spacing(2),
+        }
     }
-}})(MuiDialogContent);
+})(MuiDialogContent);
 
 const DialogActions = withStyles((theme) => ({
     root: {
@@ -45,9 +45,9 @@ const DialogActions = withStyles((theme) => ({
     }
 }))(MuiDialogActions);
 
-export default function SimpleModal (props){
+export default function SimpleModal(props) {
     const [open, setOpen] = React.useState(false);
-    const { title, children, footer, ...other } = props;
+    const {title, children, footer, width, ...other} = props;
     const handleClickOpen = () => {
         setOpen(true);
     };
@@ -55,19 +55,21 @@ export default function SimpleModal (props){
         setOpen(false);
     };
     return (
-        <Dialog onClose={props.onClose} aria-labelledby="customized-dialog-title" open={props.open}>
-            <DialogTitle id="customized-dialog-title">
+        <Dialog aria-labelledby="customized-dialog-title" open={props.open} onClose={props.onClose}>
+
+            <DialogTitle id="customized-dialog-title" width={width}>
                 {props.title}
             </DialogTitle>
+
             <DialogContent dividers>
                 {children}
             </DialogContent>
-            {props.showFooter&&(
+
+            {props.showFooter && (
                 <DialogActions>
-                {props.footer}
+                    {props.footer}
                 </DialogActions>
             )}
-            
         </Dialog>
     )
 }
