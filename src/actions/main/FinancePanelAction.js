fead79ed61fcdfe261547d7cb7b17dde588644fd
yjyvsdream <jingyuan.yu@myxxjs.com>
2021-07-15 14:31:38 +0800

    新加：【财务主控】基础文件做成

---
+++ src/actions/main/FinancePanelAction.js
@@ -0,0 +1,55 @@
+import Swal from 'sweetalert2';
+import {AppActionType, FinancePanelActionType} from '../../types';
+import {apiHost} from '../../config';
+
+const httpUtil = require('../../utils/HttpUtils');
+const localUtil = require('../../utils/LocalUtils');
+const sysConst = require('../../utils/SysConst');
+
+// 未完成的采购付款
+export const getPurchaseStat = () => async (dispatch) => {
+    try {
+        // 基本检索URL
+        let url = apiHost + '/api/user/' + localUtil.getSessionItem(sysConst.LOGIN_USER_ID)
+            + '/purchaseStat?paymentStatus=' + sysConst.PAYMENT_STATUS[0].value;
+
+        dispatch({type: AppActionType.showLoadProgress, payload: true});
+        let res = await httpUtil.httpGet(url);
+        dispatch({type: AppActionType.showLoadProgress, payload: false});
+        if (res.success) {
+            if (res.rows.length > 0) {
+                dispatch({type: FinancePanelActionType.getPurchaseStat, payload: res.rows[0]});
+            } else {
+                dispatch({type: FinancePanelActionType.getPurchaseStat, payload: {}});
+            }
+        } else if (!res.success) {
+            Swal.fire("获取采购统计信息失败", res.msg, "warning");
+        }
+    } catch (err) {
+        Swal.fire("操作失败", err.message, "error");
+    }
+};
+
+// 未完成的采购退款
+export const getPurchaseRefundStat = () => async (dispatch) => {
+    try {
+        // 基本检索URL
+        let url = apiHost + '/api/user/' + localUtil.getSessionItem(sysConst.LOGIN_USER_ID)
+            + '/purchaseRefundStat?paymentStatus=' + sysConst.REFUND_PAYMENT_STATUS[0].value;
+
+        dispatch({type: AppActionType.showLoadProgress, payload: true});
+        let res = await httpUtil.httpGet(url);
+        dispatch({type: AppActionType.showLoadProgress, payload: false});
+        if (res.success) {
+            if (res.rows.length > 0) {
+                dispatch({type: FinancePanelActionType.getPurchaseRefundStat, payload: res.rows[0]});
+            } else {
+                dispatch({type: FinancePanelActionType.getPurchaseRefundStat, payload: {}});
+            }
+        } else if (!res.success) {
+            Swal.fire("获取采购退款信息失败", res.msg, "warning");
+        }
+    } catch (err) {
+        Swal.fire("操作失败", err.message, "error");
+    }
+};
