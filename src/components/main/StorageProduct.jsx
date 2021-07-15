9ab82128bd9d2badb61b26026b7e1bc574934d1f
yjyvsdream <jingyuan.yu@myxxjs.com>
2021-07-15 15:52:01 +0800

    新加：【财务主控】基础文件做成

--- src/components/main/StorageProduct.jsx
+++ src/components/main/StorageProduct.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect, useDispatch} from 'react-redux';
 // 引入material-ui基础组件
 import {
@@ -7,6 +7,7 @@ import {
     Divider,
     Fab,
     Grid,
+    IconButton,
     makeStyles,
     Paper,
     Table,
@@ -21,6 +22,7 @@ import {
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import {DatePicker} from "@material-ui/pickers";
 import {CommonActionType, StorageProductActionType} from "../../types";
+import {SimpleModal} from "../index";
 
 const storageProductAction = require('../../actions/main/StorageProductAction');
 const commonAction = require('../../actions/layout/CommonAction');
@@ -37,21 +39,6 @@ function StorageProduct(props) {
     const classes = useStyles();
     const dispatch = useDispatch();
 
-    // 查询列表，默认第一页
-    const queryStorageProductList = () => {
-        props.getStorageProductList(0);
-    };
-
-    // 上一页
-    const getPrePage = () => {
-        props.getStorageProductList(props.storageProductReducer.storageProductData.start - (props.storageProductReducer.storageProductData.size - 1));
-    };
-
-    // 下一页
-    const getNextPage = () => {
-        props.getStorageProductList(props.storageProductReducer.storageProductData.start + (props.storageProductReducer.storageProductData.size - 1));
-    };
-
     useEffect(() => {
         // 清空reducer
         let queryParams = {
@@ -66,8 +53,67 @@ function StorageProduct(props) {
         dispatch(StorageProductActionType.setDefaultQueryParams(queryParams));
         // 取得画面 select控件，基础数据
         props.getBaseSelectList();
-        props.getStorageProductList(props.storageProductReducer.storageProductData.start);
+        dispatch(storageProductAction.getStorageProductList(storageProductReducer.storageProductData.start))
     }, []);
+
+    // 模态属性
+    const [modalOpen, setModalOpen] = React.useState(false);
+    // 模态数据
+    const [modalData, setModalData] = React.useState({storageProduct:{}});
+    // 模态校验
+    const [validation,setValidation] = useState({});
+
+    // 关闭模态
+    const closeModal = () => {
+        if (storageProductReducer.queryParams.storage != null) {
+            dispatch(commonAction.getStorageAreaList(storageProductReducer.queryParams.storage.id));
+        } else {
+            dispatch(CommonActionType.setStorageAreaList([]));
+        }
+        setModalOpen(false);
+    };
+
+    //初始添加模态框值
+    const initModal =(item) =>{
+        // 清空仓库分区
+        dispatch(CommonActionType.setStorageAreaList([]));
+        // 清check内容
+        setValidation({});
+        // 页面属性
+        setModalData({storageProduct:item,storage:null,storageArea:null,count:'',remark:''});
+        // 设定模态打开
+        setModalOpen(true);
+    };
+
+    const validate = ()=>{
+        const validateObj ={};
+        if (!modalData.storage) {
+            validateObj.storage ='请选择仓库';
+        }
+        if (!modalData.storageArea) {
+            validateObj.storageArea ='请选择仓库分区';
+        }
+        if (!modalData.count || modalData.count<1) {
+            validateObj.count ='请输入移库数量';
+        }else if(modalData.count > modalData.storageProduct.storage_count){
+            validateObj.count ='移库数量不能大于库存数量';
+        }
+        setValidation(validateObj);
+        return Object.keys(validateObj).length
+    };
+
+    const submitModal= ()=>{
+        const errorCount = validate();
+        if(errorCount===0){
+            setModalOpen(false);
+            dispatch(storageProductAction.moveProduct(modalData));
+            if (storageProductReducer.queryParams.storage != null) {
+                dispatch(commonAction.getStorageAreaList(storageProductReducer.queryParams.storage.id));
+            } else {
+                dispatch(CommonActionType.setStorageAreaList([]));
+            }
+        }
+    };
 
     return (
         <div className={classes.root}>
@@ -164,7 +210,7 @@ function StorageProduct(props) {
 
                 {/*查询按钮*/}
                 <Grid item xs={1} style={{textAlign:'right'}}>
-                    <Fab color="primary" size="small" onClick={queryStorageProductList} style={{marginTop : 30}}>
+                    <Fab color="primary" size="small" onClick={()=>{dispatch(storageProductAction.getStorageProductList(0))}} style={{marginTop : 30}}>
                         <i className="mdi mdi-magnify mdi-24px"/>
                     </Fab>
                 </Grid>
@@ -188,6 +234,7 @@ function StorageProduct(props) {
                             <TableCell className={classes.tableHead} align="center">单价</TableCell>
                             <TableCell className={classes.tableHead} align="center">库存</TableCell>
                             <TableCell className={classes.tableHead} align="center">仓储日期</TableCell>
+                            <TableCell className={classes.tableHead} align="center">操作</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -200,6 +247,11 @@ function StorageProduct(props) {
                                 <TableCell align="center">{row.unit_cost}</TableCell>
                                 <TableCell align="center">{row.storage_count}</TableCell>
                                 <TableCell align="center">{row.date_id}</TableCell>
+                                <TableCell align="center">
+                                    <IconButton color="primary" size="small" edge="start" onClick={() => {initModal(row)}}>
+                                        <i className="mdi mdi-truck"/>
+                                    </IconButton>
+                                </TableCell>
                             </TableRow>
                         ))}
                         {storageProductReducer.storageProductData.dataList.length > 0 &&
@@ -221,15 +273,85 @@ function StorageProduct(props) {
             {/* 上下页按钮 */}
             <Box style={{textAlign: 'right', marginTop: 20}}>
                 {storageProductReducer.storageProductData.start > 0 && storageProductReducer.storageProductData.dataSize > 0 &&
-                <Button variant="contained" color="primary" style={{marginRight: 20}} onClick={getPrePage}>上一页</Button>}
+                <Button variant="contained" color="primary" style={{marginRight: 20}}
+                        onClick={()=>{dispatch(storageProductAction.getStorageProductList(storageProductReducer.storageProductData.start-(storageProductReducer.storageProductData.size-1)))}}>上一页</Button>}
                 {storageProductReducer.storageProductData.dataSize >= storageProductReducer.storageProductData.size &&
-                <Button variant="contained" color="primary" onClick={getNextPage}>下一页</Button>}
+                <Button variant="contained" color="primary"
+                        onClick={()=>{dispatch(storageProductAction.getStorageProductList(storageProductReducer.storageProductData.start+(storageProductReducer.storageProductData.size-1)))}}>下一页</Button>}
             </Box>
+
+            <SimpleModal maxWidth={'sm'}
+                         title="移库"
+                         open={modalOpen}
+                         onClose={closeModal}
+                         showFooter={true}
+                         footer={
+                             <>
+                                 <Button variant="contained" color="primary" onClick={submitModal}>确定</Button>
+                                 <Button variant="contained" onClick={closeModal}>关闭</Button>
+                             </>
+                         }
+            >
+                <Grid container spacing={2}>
+                    <Grid item sm={6}>仓库：{modalData.storageProduct.storage_name}</Grid>
+                    <Grid item sm={6}>仓库分区：{modalData.storageProduct.storage_area_name}</Grid>
+                    <Grid item sm={6}>商品：{modalData.storageProduct.product_name}</Grid>
+                    <Grid item sm={6}>库存：{modalData.storageProduct.storage_count}</Grid>
+                    <Grid item sm={6}>
+                        <Autocomplete fullWidth
+                                      options={commonReducer.storageList}
+                                      getOptionLabel={(option) => option.storage_name}
+                                      value={modalData.storage}
+                                      onChange={(event, value) => {
+                                          setModalData({...modalData, storage: value, storageArea: null});
+                                          // 仓库有选择时，取得仓库分区， 否则清空
+                                          if (value != null) {
+                                              dispatch(commonAction.getStorageAreaList(value.id));
+                                          } else {
+                                              dispatch(CommonActionType.setStorageAreaList([]));
+                                          }
+                                      }}
+                                      renderInput={(params) => <TextField {...params} label="仓库" margin="dense" variant="outlined"
+                                                                          error={validation.storage&&validation.storage!=''}
+                                                                          helperText={validation.storage}
+                                      />}
+                        />
+                    </Grid>
+                    <Grid item sm={6}>
+                        <Autocomplete fullWidth
+                                      options={commonReducer.storageAreaList}
+                                      noOptionsText="无选项"
+                                      getOptionLabel={(option) => option.storage_area_name}
+                                      value={modalData.storageArea}
+                                      onChange={(event, value) => {
+                                          setModalData({...modalData, storageArea: value});
+                                      }}
+                                      renderInput={(params) => <TextField {...params} label="仓库分区" margin="dense" variant="outlined"
+                                                                          error={validation.storageArea&&validation.storageArea!=''}
+                                                                          helperText={validation.storageArea}
+                                      />}
+                        />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <TextField label="数量" fullWidth margin="dense" variant="outlined" type="number" value={modalData.count}
+                                   onChange={(e)=>{setModalData({...modalData, count: e.target.value});}}
+                                   error={validation.count&&validation.count!=''}
+                                   helperText={validation.count}
+                        />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <TextField label="备注" fullWidth margin="dense" variant="outlined" multiline rows={2} value={modalData.remark}
+                                   onChange={(e) => {setModalData({...modalData, remark: e.target.value})}}/>
+                    </Grid>
+                </Grid>
+            </SimpleModal>
         </div>
     )
 }
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state) => {
     return {
         storageProductReducer: state.StorageProductReducer,
         commonReducer: state.CommonReducer
@@ -242,9 +364,6 @@ const mapDispatchToProps = (dispatch) => ({
         dispatch(commonAction.getStorageList());
         dispatch(commonAction.getSupplierList());
         dispatch(commonAction.getProductList(null));
-    },
-    getStorageProductList: (dataStart) => {
-        dispatch(storageProductAction.getStorageProductList({dataStart}))
     },
     downLoadCsv: () => {
         dispatch(storageProductAction.downLoadCsv())
