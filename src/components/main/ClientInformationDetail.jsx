3b793b7a9901ed797d3bb13d434a36fc88b62df5
star920626 <jing.zhang@myxxjs.com>
2021-07-27 09:26:28 +0800

    客户中心-订单修改排序

--- src/components/main/ClientInformationDetail.jsx
+++ src/components/main/ClientInformationDetail.jsx
@@ -314,17 +314,24 @@ function ClientInformationDetail (props){
                                     {clientInformationDetailReducer.orderItemServiceInfo.map((service,index) => (
                                         service.order_id==row.id?
                                             <Grid>
+
+                                                {service.fixed_price=='0.00'?
                                                 <Grid container  spacing={3}>
                                                     <Grid item xs={2} align='left'>名称:{service.sale_service_name}</Grid>
-                                                    {service.fixed_price=='0.00'?<div>
-                                                            <Grid item xs={2} align='left'>单价:{service.unit_price}</Grid>
-                                                            <Grid item xs={2} align='left'>数量:{service.service_count}</Grid>
-                                                        </div>
-                                                        :<Grid item xs={2} align='left'>固定价格:{service.fixed_price}</Grid>}
+                                                    <Grid item xs={2} align='left'>单价:{service.unit_price}</Grid>
+                                                    <Grid item xs={2} align='left'>数量:{service.service_count}</Grid>
                                                     <Grid item xs={2} align='left'>总价:{service.service_price}</Grid>
                                                     <Grid item xs={2} align='left'>折扣:{service.discount_service_price}</Grid>
                                                     <Grid item xs={2} align='left'>实际价格:{service.actual_service_price}</Grid>
-                                                </Grid>
+                                                </Grid>:
+
+                                                <Grid container  spacing={3}>
+                                                    <Grid item xs={2} align='left'>名称:{service.sale_service_name}</Grid>
+                                                    <Grid item xs={2} align='left'>固定价格:{service.fixed_price}</Grid>
+                                                    <Grid item xs={2} align='left'>总价:{service.service_price}</Grid>
+                                                    <Grid item xs={2} align='left'>折扣:{service.discount_service_price}</Grid>
+                                                    <Grid item xs={2} align='left'>实际价格:{service.actual_service_price}</Grid>
+                                                </Grid>}
 
                                                 <Grid container  spacing={3}>
                                                     <Grid item xs={2} align='left'>销售:{service.sale_user_name}</Grid>
