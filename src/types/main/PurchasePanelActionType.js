6c54558e969c7ef6013b809a86d95137ab862a8c
star920626 <jing.zhang@myxxjs.com>
2021-09-03 15:21:19 +0800

    采购主控添加库存预警商品

--- src/types/main/PurchasePanelActionType.js
+++ src/types/main/PurchasePanelActionType.js
@@ -2,3 +2,7 @@ import {createAction} from 'redux-actions';
 export const getPurchaseItemStat = createAction('GET_PURCHASE_ITEM_STAT');
 export const getPurchaseRefundStat = createAction('GET_PURCHASE_REFUND_STAT');
 export const getOrderStat = createAction('GET_ORDER_STAT');
+export const getProdStoreWarning = createAction('GET_PROD_STORE_WARNING');
+export const getProdStoreWarningStart = createAction('GET_PROD_STORE_WARNING_START');
+export const getProdStoreWarningSize = createAction('GET_PROD_STORE_WARNING_SIZE');
+export const getProdStoreWarningDataSize = createAction('GET_PROD_STORE_WARNING_DATA_SIZE');
