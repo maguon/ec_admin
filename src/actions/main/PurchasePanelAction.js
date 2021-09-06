0e95eccc888e71ee7c7681a2cfb80f81373ece50
star920626 <jing.zhang@myxxjs.com>
2021-09-06 08:13:10 +0800

    修改绩效

--- src/actions/main/PurchasePanelAction.js
+++ src/actions/main/PurchasePanelAction.js
@@ -52,7 +52,29 @@ export const getPurchaseRefundStat = () => async (dispatch) => {
         Swal.fire("操作失败", err.message, "error");
     }
 };
+//未出库的订单商品
+export const getOrderStat = () => async (dispatch) => {
+    try {
+        // 基本检索URL
+        let url = apiHost + '/api/user/' + localUtil.getSessionItem(sysConst.LOGIN_USER_ID)
+            + '/orderItemProdStorage?orderItemType=' + sysConst.STORAGE_OP_TYPE[0].value;
 
+        dispatch({type: AppActionType.showLoadProgress, payload: true});
+        let res = await httpUtil.httpGet(url);
+        dispatch({type: AppActionType.showLoadProgress, payload: false});
+        if (res.success) {
+            if (res.rows.length > 0) {
+                dispatch({type: PurchasePanelActionType.getOrderStat, payload: res.count});
+            } else {
+                dispatch({type: PurchasePanelActionType.getOrderStat, payload: {}});
+            }
+        } else if (!res.success) {
+            Swal.fire("获取未出库的订单商品信息失败", res.msg, "warning");
+        }
+    } catch (err) {
+        Swal.fire("操作失败", err.message, "error");
+    }
+};
 //库存预警商品
 export const getProdStoreWarning = (params) => async (dispatch,getState) => {
     try {
