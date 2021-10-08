151b839360b6962e6c734d99387fd9db4cc0552f
star920626 <jing.zhang@myxxjs.com>
2021-10-08 14:23:14 +0800

    登陆页面修改宽度

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -36,9 +36,7 @@ const Login = (props) => {
             height: '100%',
             justifyContent: 'center',
             alignItems: "center"
-        }}
-             noValidate
-             autoComplete="off">
+        }}>
             <Container maxWidth="md" style={{paddingTop: 80}}>
                 <Typography color="textPrimary" variant="h2" align="center"><img style={{paddingTop: 6}} src="/logo120.png" alt=""/></Typography>
                 <Typography color="textPrimary" variant="h2" align="center">{webName}</Typography>
